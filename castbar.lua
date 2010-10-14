5bc0ff8e0dd50bdca0e8c57f0d9343b4c8dd7767
ceeps <adameke92@hotmail.co.uk>
2010-10-14 16:39:24 +0100

    First proper upload. LOL

---
+++ castbar.lua
@@ -0,0 +1,181 @@
+﻿local FONT = [=[Interface\AddOns\oUF_CeepsUF\media\neuropol.ttf]=]
+local TEXTURE = [=[Interface\ChatFrame\ChatFrameBackground]=]
+local STATUSTEXTURE = [=[Interface\AddOns\oUF_CeepsUF\media\Otravi]=]
+local BACKDROP = 
+{
+	bgFile = TEXTURE, insets = {top = -1, bottom = -1, left = -1, right = -1}
+}
+local BACKDROPBORDER = 
+{
+    bgFile = TEXTURE, insets = {top = -1, bottom = -1, left = -1, right = -1},
+    edgeFile = TEXTURE
+}
+
+local UnitSpecific =
+{
+    player = function(self)
+        self:SetAttribute('initial-width', 370)
+        self:SetAttribute('initial-height', 25)
+    end,
+    
+    target = function(self)
+        self:SetAttribute('initial-width', 370)
+        self:SetAttribute('initial-height', 25)
+    end
+}
+
+local PostCastStart = function(Castbar, unit, name, rank, text, castid)
+	Castbar.channeling = false
+	if unit == "vehicle" then unit = "player" end
+
+	if unit == "player" then
+		local latency = GetTime() - Castbar.castSent
+		latency = latency > Castbar.max and Castbar.max or latency
+		Castbar.Latency:SetText(("%dms"):format(latency * 1e3))
+		Castbar.SafeZone:SetWidth(Castbar:GetWidth() * latency / Castbar.max)
+		Castbar.SafeZone:ClearAllPoints()
+		Castbar.SafeZone:SetPoint("TOPRIGHT")
+		Castbar.SafeZone:SetPoint("BOTTOMRIGHT")
+	end
+end
+
+local PostChannelStart = function(Castbar, unit, name, rank, text)
+	Castbar.channeling = true
+	if unit == "vehicle" then unit = "player" end
+
+	if unit == "player" then
+		local latency = GetTime() - Castbar.castSent
+		latency = latency > Castbar.max and Castbar.max or latency
+		Castbar.Latency:SetText(("%dms"):format(latency * 1e3))
+		Castbar.SafeZone:SetWidth(Castbar:GetWidth() * latency / Castbar.max)
+		Castbar.SafeZone:ClearAllPoints()
+		Castbar.SafeZone:SetPoint("TOPLEFT")
+		Castbar.SafeZone:SetPoint("BOTTOMLEFT")
+	end
+
+end
+
+local CustomCastTimeText = function(self, duration)
+	self.Time:SetText(("%.1f / %.1f"):format(self.channeling and duration or self.max - duration, self.max))
+end
+
+local CustomCastDelayText = function(self, duration)
+	self.Time:SetText(("%.1f |cffaf5050%s %.1f|r"):format(self.channeling and duration or self.max - duration, self.channeling and "- " or "+", self.delay))
+end
+
+local FormatTime = function(s)
+	local day, hour, minute = 86400, 3600, 60
+	if s >= day then
+		return format("%dd", floor(s/day + 0.5)), s % day
+	elseif s >= hour then
+		return format("%dh", floor(s/hour + 0.5)), s % hour
+	elseif s >= minute then
+		if s <= minute * 5 then
+			return format("%d:%02d", floor(s/60), s % minute), s - floor(s)
+		end
+		return format("%dm", floor(s/minute + 0.5)), s % minute
+	elseif s >= minute / 12 then
+		return floor(s + 0.5), (s * 100 - floor(s * 100))/100
+	end
+	return format("%.1f", s), (s * 100 - floor(s * 100))/100
+end
+
+local function Shared(self, unit)
+
+    if (unit == 'player' or unit == 'target') then
+
+        self:RegisterForClicks('AnyUp')
+	    self:SetScript('OnEnter', UnitFrame_OnEnter)
+	    self:SetScript('OnLeave', UnitFrame_OnLeave)
+        
+        self.FrameBackdrop = CreateFrame("Frame", nil, self)
+	    self.FrameBackdrop:SetPoint("TOPLEFT", self, -30, 8)
+        self.FrameBackdrop:SetPoint("BOTTOMRIGHT", self, 5, -8)
+	    self.FrameBackdrop:SetFrameStrata("BACKGROUND")
+	    self.FrameBackdrop:SetBackdrop(BACKDROPBORDER)
+	    self.FrameBackdrop:SetBackdropColor(0, 0, 0, 0)
+	    self.FrameBackdrop:SetBackdropBorderColor(0, 0, 0, 0.3)
+
+        
+        local castbar = CreateFrame("StatusBar", nil, self)
+	    castbar:SetStatusBarTexture(TEXTURE)
+	    castbar:SetStatusBarColor(1/7, 1/7, 1/7)
+	    castbar:SetAllPoints()
+	    castbar:SetToplevel(true)
+        castbar:SetFrameStrata("HIGH")
+        self.Castbar = castbar
+        
+        castbar.PostCastStart = PostCastStart
+		castbar.PostChannelStart = PostChannelStart
+        
+        local castbarBG = castbar:CreateTexture(nil, 'BORDER')
+	    castbarBG:SetAllPoints()
+        castbarBG:SetTexture(TEXTURE)
+	    castbarBG:SetTexture(1/3, 1/3, 1/3)
+        
+        castbar.Icon = castbar:CreateTexture(nil, "ARTWORK")
+        castbar.Icon:SetSize(25, 25)
+		castbar.Icon:SetTexCoord(0, 1, 0, 1)
+		if unit == "player" then
+			castbar.Icon:SetPoint("LEFT", -25, 0)
+		elseif unit == "target" then
+			castbar.Icon:SetPoint("LEFT", -25, 0)
+		end
+        
+        self.IconBackdrop = CreateFrame("Frame", nil, castbar)
+		self.IconBackdrop:SetPoint("TOPLEFT", castbar.Icon, -1, 1)
+		self.IconBackdrop:SetPoint("BOTTOMRIGHT", castbar.Icon, 1, -1)
+		self.IconBackdrop:SetBackdrop(BACKDROP)
+		self.IconBackdrop:SetBackdropColor(0, 0, 0, 0)
+		self.IconBackdrop:SetBackdropBorderColor(0, 0, 0, 0.7)
+        
+        castbar.Time = castbar:CreateFontString(nil, "OVERLAY")
+        castbar.Time:SetFont(FONT, 10, "OUTLINE")
+		castbar.Time:SetPoint("RIGHT", -1, 1)
+		castbar.Time:SetTextColor(1, 1, 1)
+		castbar.Time:SetJustifyH("LEFT")
+		castbar.CustomTimeText = CustomCastTimeText
+		castbar.CustomDelayText = CustomCastDelayText
+        
+        castbar.Text = castbar:CreateFontString(nil, "OVERLAY")
+        castbar.Text:SetFont(FONT, 12, "OUTLINE")
+		castbar.Text:SetPoint("LEFT", 1, 1)
+		castbar.Text:SetTextColor(1, 1, 1)
+
+		castbar:HookScript("OnShow", function() castbar.Text:Show(); castbar.Time:Show(); self.FrameBackdrop:Show() end)
+		castbar:HookScript("OnHide", function() castbar.Text:Hide(); castbar.Time:Hide(); self.FrameBackdrop:Hide() end)
+
+        
+        if unit == "player" then
+			castbar.SafeZone = castbar:CreateTexture(nil, "ARTWORK")
+			castbar.SafeZone:SetTexture(TEXTURE)
+			castbar.SafeZone:SetVertexColor(0.69, 0.31, 0.31, 0.75)
+
+			castbar.Latency = castbar:CreateFontString(nil, "OVERLAY")
+			castbar.Latency:SetFont(FONT, 9, "OUTLINE")
+			castbar.Latency:SetTextColor(1, 1, 1)
+			castbar.Latency:SetPoint("BOTTOMLEFT", castbar.SafeZone, "BOTTOMRIGHT")
+			
+			self:RegisterEvent("UNIT_SPELLCAST_SENT", function(self, event, caster)
+				if caster == "player" or caster == "vehicle" then
+					castbar.castSent = GetTime()
+				end
+			end)
+		end
+   
+    end
+    
+    if(UnitSpecific[unit]) then
+		return UnitSpecific[unit](self)
+	end
+    
+end
+
+oUF:RegisterStyle('castbar', Shared)
+oUF:Factory(function(self)
+	self:SetActiveStyle('castbar')
+	self:Spawn('player'):SetPoint('CENTER', 11, -410)
+    self:Spawn('target'):SetPoint('CENTER', 11, -375)
+
+end)
+    
---
+++ oUF_CeepsUF.lua
@@ -0,0 +1,377 @@
+﻿--textures and shizzle
+
+local FONT = [=[Interface\AddOns\oUF_CeepsUF\media\neuropol.ttf]=]
+local TEXTURE = [=[Interface\ChatFrame\ChatFrameBackground]=]
+local STATUSTEXTURE = [=[Interface\AddOns\oUF_CeepsUF\media\Otravi]=]
+local BACKDROP = 
+{
+	bgFile = TEXTURE, insets = {top = -1, bottom = -1, left = -1, right = -1}
+}
+local BACKDROPBORDER =
+{
+    bgFile = TEXTURE, insets = {top = -1, bottom = -1, left = -1, right = -1},
+    edgeFile = TEXTURE
+}
+
+local playerClass = string.upper(select(2, UnitClass('player')))
+
+
+oUF.Tags['CeepsUF:health'] = function(unit)
+	local min, max = UnitHealth(unit), UnitHealthMax(unit)
+    local percent = min * 100 / max
+	local status = not UnitIsConnected(unit) and 'Offline' or UnitIsGhost(unit) and 'Ghost' or UnitIsDead(unit) and 'Dead'
+
+	if(status) then
+		return status
+	elseif(unit == 'player' and min ~= max) then
+		return min
+	elseif(unit == 'target' and min ~= max) then
+		return min
+    elseif(unit == 'targettarget') then
+        return ('%d%%'):format(min / max * 100)
+	else
+		return max
+	end
+end
+
+oUF.Tags['CeepsUF:power'] = function(unit)
+	local power = UnitPower(unit)
+	if(power >= 0 and not UnitIsDeadOrGhost(unit)) then
+		local _, type = UnitClass(unit)
+		local colors = _COLORS.class
+		return ('%s%d|r'):format(Hex(colors[type] or colors['RUNES']), power)
+	end
+end
+	
+local function SpawnMenu(self)
+	ToggleDropDownMenu(1, nil, _G[string.gsub(self.unit, '^.', string.upper)..'FrameDropDown'], 'cursor')
+end
+--units
+
+local function PostCreateAura(element, button)
+	button:SetBackdrop(BACKDROP)
+	button:SetBackdropColor(0, 0, 0)
+	button.cd:SetReverse()
+	button.icon:SetTexCoord(0.08, 0.92, 0.08, 0.92)
+	button.icon:SetDrawLayer('ARTWORK')
+end
+
+local UnitSpecific = 
+{
+	player = function(self)
+        self:SetAttribute('initial-width', 254)   
+        
+    end,
+    
+    target = function(self)
+        self:SetAttribute('initial-width', 254)
+        
+        local Buffs = CreateFrame("Frame", nil, self)
+	    Buffs:SetPoint('BOTTOM', self, 'TOP')
+	    Buffs:SetPoint'LEFT'
+	    Buffs:SetPoint'RIGHT'
+	    Buffs:SetHeight(21)
+        Buffs.PostCreateIcon = PostCreateAura
+        Buffs.showBuffType = true
+
+	    Buffs.size = 21
+	    Buffs.num = math.floor(self:GetAttribute'initial-width' / Buffs.size + .5)
+
+	    self.Buffs = Buffs
+        
+        local Debuffs = CreateFrame("Frame", nil, self)
+	    Debuffs:SetPoint("BOTTOM", self, "TOP", 0, 21)
+	    Debuffs:SetPoint'LEFT'
+	    Debuffs:SetPoint'RIGHT'
+	    Debuffs:SetHeight(21)
+        Debuffs.PostCreateIcon = PostCreateAura
+
+	    Debuffs.initialAnchor = "TOPLEFT"
+	    Debuffs.size = 21
+	    Debuffs.showDebuffType = true
+	    Debuffs.num = math.floor(self:GetAttribute'initial-width' / Debuffs.size + .5)
+
+	    self.Debuffs = Debuffs
+    end,
+    
+    targettarget = function(self)
+    
+        self:SetAttribute('initial-width', 160)
+    end,
+       
+    party = function(self)
+        
+        self:SetAttribute('initial-width', 100)
+    end
+    
+}
+
+--main code
+local function Shared(self, unit)
+
+	self:RegisterForClicks('AnyUp')
+	self:SetScript('OnEnter', UnitFrame_OnEnter)
+	self:SetScript('OnLeave', UnitFrame_OnLeave)
+
+	self:SetBackdrop(BACKDROP)
+	self:SetBackdropColor(0, 0, 0)    
+
+    --health bar
+
+	local health = CreateFrame('StatusBar', nil, self)
+    health:SetStatusBarTexture(TEXTURE)
+	health:SetStatusBarColor(1/7, 1/7, 1/7)
+    health:SetFrameStrata("MEDIUM")
+	health.frequentUpdates = true
+	self.Health = health
+    
+    local healthBG = health:CreateTexture(nil, 'BORDER')
+	healthBG:SetAllPoints()
+    healthBG:SetTexture(TEXTURE)
+	healthBG:SetTexture(1/3, 1/3, 1/3)
+    
+    local healthValue = health:CreateFontString(nil, 'OVERLAY')
+	healthValue:SetFont(FONT, 26, 'OUTLINE')
+    healthValue:SetTextColor(1, 1, 1)
+	healthValue.frequentUpdates = 0.1
+	self:Tag(healthValue, '[CeepsUF:health]')
+    
+    local powerValue = health:CreateFontString(nil, 'OVERLAY')
+	powerValue:SetFont(FONT, 24, 'OUTLINE')
+	powerValue.frequentUpdates = 0.1
+	self:Tag(powerValue, '[CeepsUF:power]') 
+    
+    --player frame
+
+	if(unit == 'player' or unit == 'target') then
+		local power = CreateFrame('StatusBar', nil, self)
+		power:SetPoint('BOTTOMRIGHT')
+		power:SetPoint('BOTTOMLEFT')
+		power:SetPoint('TOP', health, 'BOTTOM', 0, -1)
+        power:SetStatusBarTexture(TEXTURE)
+		power.frequentUpdates = true
+		self.Power = power
+
+		power.colorClass = true
+		power.colorTapping = true
+		power.colorDisconnected = true
+		
+		if(unit == 'target' and not UnitIsPlayer(unit)) then
+			power.colorReaction = true
+		end
+        
+        local powerbg = power:CreateTexture(nil, 'BORDER')
+		powerbg:SetAllPoints()
+		powerbg:SetTexture(TEXTURE)
+		powerbg.multiplier = 1/3
+		power.bg = powerbg
+
+		health:SetHeight(38)
+		health:SetPoint('TOPRIGHT')
+		health:SetPoint('TOPLEFT')
+        
+        healthValue:SetPoint('LEFT', health, -2, -45)
+        healthValue:SetJustifyH('RIGHT')
+        powerValue:SetPoint('LEFT', health, -2, -73)
+        powerValue:SetJustifyH('LEFT')
+        
+        --name
+        
+        local Name = health:CreateFontString(nil, "OVERLAY")
+	    Name:SetPoint("LEFT", -302, -1)
+	    Name:SetPoint("RIGHT", -265, -1)
+	    Name:SetJustifyH("RIGHT")
+	    Name:SetFont(FONT, 26, 'OUTLINE')
+        Name:SetTextColor(1, 1, 1) --TODO change the colour so that it matches the class colour
+        
+        self:Tag(Name, '[name]')
+	    self.Name = Name
+        
+        if (unit == 'target') then
+            healthValue:SetPoint('RIGHT', health, -2, -45)
+            powerValue:SetPoint('RIGHT', health, -2, -73)
+            powerValue:SetJustifyH('RIGHT')
+            Name:SetJustifyH("LEFT")
+            Name:SetPoint("LEFT", 265, -1)
+            Name:SetPoint("RIGHT", 302, -1)
+        end
+        
+        if IsAddOnLoaded("oUF_CombatFeedback") then
+        
+        	local combatfeedback = health:CreateFontString(nil, "OVERLAY")
+	        combatfeedback:SetPoint("CENTER", health, "CENTER")
+	        combatfeedback:SetFontObject(GameFontNormal)
+	        self.CombatFeedbackText = combatfeedback
+        end
+                
+        --menu
+
+		self.menu = SpawnMenu
+		self:SetAttribute('type2', 'menu')
+		self:SetAttribute('initial-height', 50)
+
+    elseif (unit == 'party') then
+    
+		local power = CreateFrame('StatusBar', nil, self)
+		power:SetPoint('BOTTOMRIGHT')
+		power:SetPoint('BOTTOMLEFT')
+		power:SetPoint('TOP', health, 'BOTTOM', 0, -1)
+        power:SetStatusBarTexture(TEXTURE)
+		power.frequentUpdates = true
+		self.Power = power
+
+		power.colorClass = true
+		power.colorDisconnected = true
+        
+        local powerbg = power:CreateTexture(nil, 'BORDER')
+		powerbg:SetAllPoints()
+		powerbg:SetTexture(TEXTURE)
+		powerbg.multiplier = 1/3
+		power.bg = powerbg
+
+		health:SetHeight(20)
+		health:SetPoint('TOPRIGHT')
+		health:SetPoint('TOPLEFT')
+
+		self:SetAttribute('initial-height', 30)
+        
+        local Name = health:CreateFontString(nil, "OVERLAY")
+	    Name:SetPoint("LEFT", 0, -1)
+	    Name:SetPoint("RIGHT", 0, -1)
+	    Name:SetJustifyH("LEFT")
+	    Name:SetFont(FONT, 10, 'OUTLINE')
+        Name:SetTextColor(1, 1, 1) --TODO change the colour so that it matches the class colour
+        
+        self:Tag(Name, '[name]')
+	    self.Name = Name
+        
+    elseif (unit == 'targettarget') then
+
+		health:SetHeight(30)
+		health:SetPoint('TOPRIGHT')
+		health:SetPoint('TOPLEFT')
+        health.colorClass = true
+
+		self:SetAttribute('initial-height', 30)
+        
+        local healthValue = health:CreateFontString(nil, 'OVERLAY')
+	    healthValue:SetFont(FONT, 14, 'OUTLINE')
+        healthValue:SetTextColor(1, 1, 1)
+	    healthValue.frequentUpdates = 0.1
+        healthValue:SetPoint('RIGHT', health, 50, 0)
+        healthValue:SetJustifyH('RIGHT')
+
+	    self:Tag(healthValue, '[CeepsUF:health]')        
+        
+        local Name = health:CreateFontString(nil, "OVERLAY")
+	    Name:SetPoint("LEFT", 0, -1)
+	    Name:SetPoint("RIGHT", 0, -1)
+	    Name:SetJustifyH("LEFT")
+	    Name:SetFont(FONT, 14, 'THINOUTLINE')
+        Name:SetTextColor(1, 1, 1) --TODO change the colour so that it matches the class colour
+        
+        self:Tag(Name, '[name]')
+	    self.Name = Name
+
+
+	end
+    
+    if (unit == 'player' or unit == 'target' or unit == 'party' or unit == 'targettarget') then
+    
+    self.FrameBackdrop = CreateFrame("Frame", nil, self)
+	self.FrameBackdrop:SetPoint("TOPLEFT", self, -5, 4)
+    self.FrameBackdrop:SetPoint("BOTTOMRIGHT", self, 5, -4)
+	self.FrameBackdrop:SetBackdrop(BACKDROPBORDER)
+    self.FrameBackdrop:SetFrameStrata("BACKGROUND")
+	self.FrameBackdrop:SetBackdropColor(0, 0, 0, 0)
+	self.FrameBackdrop:SetBackdropBorderColor(0, 0, 0, 0.3)
+    
+    if (unit == 'player' and playerClass == 'SHAMAN') then
+    self.FrameBackdrop:SetPoint("TOPLEFT", self, -5, 15)
+    end
+	
+    end
+
+	--SHAMAN--
+    
+    if IsAddOnLoaded("oUF_TotemBar") and unit == "player"  and playerClass == "SHAMAN" then
+	self.TotemBar = {}
+	for i = 1, 4 do
+		self.TotemBar[i] = CreateFrame("StatusBar", nil, self)
+		self.TotemBar[i]:SetHeight(9)
+		self.TotemBar[i]:SetWidth(254/4)
+		if (i == 1) then
+			self.TotemBar[i]:SetPoint("BOTTOMLEFT", self, "TOPLEFT", 0, 1)
+		else
+			self.TotemBar[i]:SetPoint("BOTTOMLEFT", self.TotemBar[i-1], "BOTTOMRIGHT", 0, 0)
+		end
+		self.TotemBar[i]:SetStatusBarTexture(TEXTURE)
+		self.TotemBar[i]:SetBackdrop(BACKDROP)
+		self.TotemBar[i]:SetBackdropColor(0, 0, 0)
+		self.TotemBar[i]:SetMinMaxValues(0, 1)
+						
+		self.TotemBar[i].bg = self.TotemBar[i]:CreateTexture(nil, "BORDER")
+		self.TotemBar[i].bg:SetAllPoints(self.TotemBar[i])
+		self.TotemBar[i].bg:SetTexture(TEXTURE)
+		self.TotemBar[i].bg.multiplier = 0.25
+
+	    end
+    end     
+
+	--DEATH KNIGHT--
+
+	if(unit == 'player' and playerClass == "DEATHKNIGHT") then
+		self.Runes = CreateFrame("Frame", nil, self)
+		self.Runes:SetPoint("BOTTOMLEFT", self, "TOPLEFT", -1, 1)
+		self.Runes:SetHeight(9)
+		self.Runes:SetWidth(254)
+		self.Runes:SetBackdrop(BACKDROP)
+		self.Runes:SetBackdropColor(0, 0, 0)
+
+		for i = 1, 6 do
+			self.Runes[i] = CreateFrame("StatusBar", self:GetName().."_Runes"..i, self)
+			self.Runes[i]:SetSize(((254 - 5) / 6), 9)
+			if (i == 1) then
+				self.Runes[i]:SetPoint("BOTTOMLEFT", self, "TOPLEFT", 0, 1)
+			else
+				self.Runes[i]:SetPoint("LEFT", self.Runes[i-1], "RIGHT", 1, 0)
+			end
+			self.Runes[i]:SetStatusBarTexture(TEXTURE)
+
+			self.Runes[i].bd = self.Runes[i]:CreateTexture(nil, "BORDER")
+			self.Runes[i].bd:SetAllPoints()
+			self.Runes[i].bd:SetTexture(TEXTURE)
+			self.Runes[i].bd:SetVertexColor(0.15, 0.15, 0.15)
+		end
+	end
+    
+        if(UnitSpecific[unit]) then
+		return UnitSpecific[unit](self)
+	end
+end
+
+oUF:RegisterStyle('CeepsUF', Shared)
+oUF:Factory(function(self)
+	self:SetActiveStyle('CeepsUF')
+	self:Spawn('player'):SetPoint('CENTER', -170, -280)
+    self:Spawn('target'):SetPoint('CENTER', 170, -280)
+    self:Spawn('targettarget'):SetPoint('CENTER', 300, -195)
+    
+    local party = self:SpawnHeader(nil, nil, 'party',
+		'showParty', true,
+		'yOffset', -20,
+		'xOffset', -20,
+		'maxColumns', 1,
+		'unitsPerColumn', 4,
+		'columnAnchorPoint', 'LEFT',
+		'columnSpacing', 0
+	)
+    party:SetPoint('TOPLEFT', 20, -350)
+
+    
+end)
+
+CompactRaidFrameManager:UnregisterAllEvents()
+CompactRaidFrameManager:Hide() 
+CompactRaidFrameContainer:UnregisterAllEvents() 
+CompactRaidFrameContainer:Hide() 
